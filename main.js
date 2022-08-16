711aa9273fca8809810a2e09840162e6a347e56f
edward-vera <103536879+edward-vera@users.noreply.github.com>
2022-08-15 21:26:53 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,56 +1,55 @@
-let arrayOfPosts;
-let fivePosts;
-let arrayOfComments;
-let arrayOfUserIds;
+// Store variable in an arr
+let arrayOfPeople;
 
 // This function waits for the web page to be loaded, when it does it will run the code inside of it which happens to be getPosts()
 window.onload = function() {
-  getPosts();
-  getFivePosts();
+  getPeople();
 }
 
-// This function is going to make a fetch request to the URL inside its parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
-const getPosts = () => {
-  fetch('https://randomuser.me/api/')
-    .then(res => res.json())
-    .then(posts => arrayOfPosts = posts)
+const getPeople = () => {
+  fetch('https://randomuser.me/api/?results=5')
+    .then((res) => res.json())
+    // .then(people => console.log(people))
+    .then((address) => (arrayOfPeople = address.results))
 }
 
 // This function logs the results in your browser's console
-const consolePosts = () => {
-  console.log(arrayOfPosts)
-  console.log(arrayOfFivePosts)
-}
-
-// this function creates elements inside the all-posts ul, then appends text inside it with the posts that were returned in the request.
-// Fetch all posts
-const displayPost = () => {
-  const allPosts = document.getElementById('all-posts')
-  arrayOfPosts.map((post, index) => {
-    const li = document.createElement('li')
-    const text = document.createTextNode(`#${index}, Title: ${post.title}:  ${post.body}, by user: ${post.userId}`)
-    li.appendChild(text)
-    allPosts.append(li)
-  });
+const consolePeople = () => {
+  console.log(arrayOfPeople);
+//   console.log(dataArray.results[0]);
+//   console.log(dataArray.results[0].gender);
+//   console.log(dataArray.results[0].name);
+//   console.log(dataArray.results[0].location);
+//   console.log(dataArray.results[0].phone);
+//   console.log(dataArray.results[0].email);
+//   console.log(dataArray.results[0].picture.thumbnail);
 };
 
-// Fetch 5 Posts
-const getFivePosts = () => {
-    fetch('http://jsonplaceholder.typicode.com/posts')
-    .then((res) => res.json())
-    .then((posts) => (arrayOfFivePosts = posts.slice(0, 5)));
-};
-
-const displayFivePost = () => {
-    const showFivePosts = document.getElementById('five-posts')
-    arrayOfFivePosts.map((post, index) => {
-        const li = `<li>#${index + 1}, Title: ${post.title}: ${post.body}, by user: ${post.userId}</li>`;
-            showFivePosts.insertAdjacentHTML('beforeend', li);
+const getPerson = (array) => {
+    array.map((people) => {
+        const displayPerson = document.getElementById('page-container');
+        const html = `
+        <div id="card-container">
+        <picture>
+            <img src="${people.picture.thumbnail}" alt="user image" class="user-image" />
+        </picture>
+        <h1>${people.name.first} ${people.name.last}</h1>
+        <p>(${people.gender})</p>
+        <h6>${people.email}</h6>
+        <h6>${people.phone}: Home</h6>
+        <h6>${people.cell}: Cell</h6>
+        <h6>${people.location.street.number.name}</h6>
+        <h6>${people.location.city}</h6>
+        <h6>${people.location.state}</h6>
+        <h6>${people.location.postcode}</h6>
+        </div>
+        </div>
+        </div>
+        `;
+        displayPerson.insertAdjacentHTML('afterbegin', html)
     });
 };
-/* 
-Your job now is to follow the functions above and use them as templates 
- to build the functionality the buttons in the index.html file already 
- have laid out in it. This way you can learn how to build fetch requests 
- and work with other APIs and become a real developer!! 
-*/
+
+const displayPeople = () => {
+    getPerson(arrayOfPeople);
+};
